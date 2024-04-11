ab3d07433d3bc4d47b3f8e996884d1fbe5a3c213
Varsha-Narwariya <165024874+Varsha-Narwariya@users.noreply.github.com>
2024-04-11 17:21:53 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,62 @@
+</<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>BANKING SYSTEM</title>
+<link rel="stylesheet" href="style_about.css">
+  </head>
+
+  <body>
+<div class="navbar">
+<center>
+    <hr color="grey" width=70% size=1>
+  <h1> BASIC BANKING SYSTEM </h1>
+  <a href="index.php">Home</a>
+  <a href="sendmoney.php">Send Money</a>
+  <a href="customers.php">View All Customers</a>
+  <a href="transactions.php">Transactions</a>
+  <a class="active" href="contact.php">Contact Us</a>
+  <a href="about.php">About Us</a>
+
+      <hr color="grey" width=70% size=1>
+      </center>
+</div>
+
+<div class="container">
+<div class="header"> Welcome to Sparks Bank! </div>
+<img src="bank.png" height=65% width=30% alt="Welcome to the sparks bank!" style="padding: 5px; margin-right: 8vw; margin-top:8vh; float:right"> 
+</div>
+  <center>
+  <div class="contentbox">
+  
+    <h1> CONTACT US</h1>
+    <img src="phone.jpg" alt="Contact Us" width="50px" height="50px" style="
+  vertical-align: middle; border-radius: 50%;border-style: double; margin:2%;">
+    <h5> +91 8489xxxxxx </h5>
+    <br>
+    
+    <img src="email.jpg" alt="Contact Us" width="50px" height="50px" 
+   style="vertical-align: middle; border-radius: 50%;border-style: double; margin:2%;">
+    <h5>  varshanarwariya1@gmail.com </h5>
+    <br>
+    <img src="address.jpg" alt="Contact Us" width="50px" height="50px" style="
+  vertical-align: middle; border-radius: 50%;border-style: double; margin:2%;;"> 
+    <h5> GWALIOR, India</h5>
+    <br>
+
+      <center>
+      <div class="mapouter"><div class="gmap_canvas"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3578.9679721489747!2d78.20457707512296!3d26.23023018908715!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3976c14c64938e5f%3A0x87b3d6a725f7b077!2sMadhav%20Institute%20of%20Technology%20%26%20Science%2C%20Gwalior!5e0!3m2!1sen!2sin!4v1712488352994!5m2!1sen!2sin" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div><style>.mapouter{position:relative;text-align:center;width:100%;height:400px;padding-left:10%;}.gmap_canvas {overflow:hidden;background:none!important;width:80%;height:400px;}.gmap_iframe {height:400px!important;}</style></div>       
+      </center>
+        <br> <br>
+  </div>
+  </center>
+
+<div class="pagebreak">
+</div>
+<div class="footer"> <center>
+Made by varsha under TSF
+</center>
+</div>
+</center>
+  </body>
+</html>
